f10811ea35295b71e2c8692aa39dbd7b857ce6ee
Brevno78 <168448563+Brevno78@users.noreply.github.com>
2024-11-17 11:22:29 +0300

    test 1 py

--- test/test_1.py
+++ test/test_1.py
@@ -1,7 +1,11 @@
 def foo():
     for i in range(0, 10):
-        print(0)
-
+        cond = (i % 2 == 0)    
+        print(f"i = {i}, cond = {cond}")
+        if cond % 2 == 0:
+            print("четное")
+        else:
+            print("нечетное")
 
 if __name__ == "__main__":
     foo()
